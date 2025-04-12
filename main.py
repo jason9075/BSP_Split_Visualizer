db75e2e0dc4ba637694552d879b64c55e9eff5ee
jason9075 <jason9075@users.noreply.github.com>
2025-04-12 15:27:58 +0800

    update: let user choose map

--- main.py
+++ main.py
@@ -22,25 +22,41 @@ def load_segments_from_file(filename: str):
 
 
 def main():
-    # segments = load_segments_from_file("files/test.txt")
-    # segments = load_segments_from_file("files/de_dust2.txt")
-    segments = load_segments_from_file("files/e1m1.txt")
+    # ask which file to load
+    print("Select a file to load:\n")
+    print("1. files/test.txt")
+    print("2. files/de_dust2.txt")
+    print("3. files/e1m1.txt")
+
+    choice = input("Enter your choice (1/2/3): ")
+
+    if choice == "1":
+        player_loc = Point(1.4, 1.6)
+        segments = load_segments_from_file("files/test.txt")
+    elif choice == "2":
+        player_loc = Point(379, 2193)
+        segments = load_segments_from_file("files/de_dust2.txt")
+    elif choice == "3":
+        player_loc = Point(1056, -3616)
+        segments = load_segments_from_file("files/e1m1.txt")
+    else:
+        print("Invalid choice. Exiting.")
+        return
 
     # Visualize the segments and the BSP tree
     visualizer = Visualizer(segments)
 
-    visualizer.render_map()
+    # visualizer.render_map()
 
-    # bsp = BSP(segments, max_depth=20, min_segments=10)
-    # bsp.build(method="score")
+    bsp = BSP(segments, max_depth=20, min_segments=10)
+    bsp.build(method="score")
 
     # visualizer.animate_split(bsp.steps, interval=100)
 
     # positions = bsp.layout_bsp_tree(bsp.root)
     # visualizer.draw_bsp_tree(bsp.root, positions, show_text=False)
 
-    player_loc = Point(1.4, 1.6)
-    # visualizer.render_sectors(player_loc, bsp.root)
+    visualizer.render_sectors(player_loc, bsp.root)
 
     visualizer.show()
 
